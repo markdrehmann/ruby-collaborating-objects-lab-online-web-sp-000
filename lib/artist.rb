dfe51c78226bd289ed7fce9287ed5cbfa0709519
Mark Drehmann <markdrehmann@gmail.com>
2020-03-05 00:05:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,6 +9,17 @@ class Artist
     @@all << self
   end
 
+  def add_song(name)
+
+  end
+
+  def artist_name(name)
+    if song.artist.nil?
+      Artist.new(name)
+    else
+      song.artist.name = name
+    end
+
   def self.all
     @@all
   end
