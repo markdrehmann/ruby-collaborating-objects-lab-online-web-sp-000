dd69b30cad8b0b5ad55a9b825cac209bd3e9d7b5
Mark Drehmann <markdrehmann@gmail.com>
2020-03-05 03:18:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,12 @@ class Song
     @@all << self
   end
 
+  def new_by_filename(filename)
+    filename.split(" - ")
+    song = Song.new(filename[1])
+    song.artist = filename[0]
+  end
+
   def self.all
     @@all
   end
